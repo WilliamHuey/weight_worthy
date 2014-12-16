b27a13ebe106fd4506489fd124d91a4d8f10e667
Binh Tran <btbinhtran@gmail.com>
2014-12-16 04:53:28 +0000

    User is able to delete workout set from add workout page if there is more than 1

--- client/templates/workouts/add_set.js
+++ client/templates/workouts/add_set.js
@@ -1,9 +1,15 @@
- Template.addWorkout.events({
-   'click .add-set': function(e, template) {
-     e.preventDefault();
+Template.addWorkout.events({
+  'click .add-set': function(e, template) {
+    e.preventDefault();
     Blaze.render(
       Template.addSet,
       $(e.target).parents('.sets').get(0)
     );
+  },
+  'click .del-set': function(e) {
+    e.preventDefault();
+    if($('.set').size() > 1) {
+      $(e.target).parents('.set').remove();
+    }
   }
- });
+});
