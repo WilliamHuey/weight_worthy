733e8932bb0d6dae9b78665c8ebeaff765b4011d
Binh Tran <btbinhtran@gmail.com>
2014-12-16 04:33:11 +0000

    User is able to delete exercise from add workout page if there is more than 1

--- client/templates/workouts/add_workout_exercise.js
+++ client/templates/workouts/add_workout_exercise.js
@@ -1,6 +1,13 @@
 Template.addWorkoutExercise.events({
   'click .add-exercise': function(e) {
-    Blaze.render( Template.addWorkoutExercise , $( '#exercises-area' ).get(0) );
     e.preventDefault();
+    Blaze.render( Template.addWorkoutExercise , $( '#exercises-area' ).get(0) );
+  },
+  
+  'click .del-exercise': function(e) {
+    e.preventDefault();
+    if ($('.exercise').size() > 1) {
+      $(e.target).parents('.exercise').remove();  
+    }
   }
 });
