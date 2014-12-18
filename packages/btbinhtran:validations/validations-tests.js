430d3b57b4cd3aac6dd0efc8a0d8b71ccbab266e
Binh Tran <btbinhtran@gmail.com>
2014-12-18 06:29:44 +0000

    Test is empty validation

--- packages/btbinhtran:validations/validations-tests.js
+++ packages/btbinhtran:validations/validations-tests.js
@@ -1,5 +1,12 @@
 // Write your tests here!
 // Here is an example.
-Tinytest.add('example', function (test) {
-  test.equal(true, true);
+Tinytest.add('is empty', function (test) {
+  var blankVal = '';
+  var errorMessages = Val.validate(blankVal, {is: {notEmpty: true}}, 'Blank');
+  var actualMessages = [{
+    "errorMessage": "Blank is empty."
+  }];
+  test.equal(errorMessages, actualMessages);
 });
+
+
