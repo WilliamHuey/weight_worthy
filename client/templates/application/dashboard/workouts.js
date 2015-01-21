b9a578cf871d318878f621338808315ae71d7472
WilliamHuey <lliw1236@yahoo.com>
2015-01-21 01:24:24 -0800

    Add margins around workout dashboard

--- client/templates/application/dashboard/workouts.js
+++ client/templates/application/dashboard/workouts.js
@@ -82,12 +82,16 @@ Template.workouts.rendered = function() {
       }
     }
   }
+
+  var margin = {top: 20, right: 20, bottom: 60, left: 20};
   
   //console.log(dates);
 
   var $chart = $('#chart'),
-    chartWidth = $chart.width(),
-    chartHeight = $chart.height(),
+    totalWidth = $chart.width()
+    chartWidth = totalWidth - margin.right,
+    totalHeight = $chart.height(),
+    chartHeight = totalHeight - margin.top - margin.bottom ,
     stack = d3.layout.stack(),
     colors = d3.scale.category10();
 
@@ -95,7 +99,11 @@ Template.workouts.rendered = function() {
   
   //console.log('dataset ', JSON.stringify(dataSet));
 
-  var svg = d3.select("#chart").append("svg");
+  var svg = d3.select("#chart").append("svg")  
+  .append("g")
+  .attr("transform", "translate(" + (margin.left + margin.right) + "," + margin.top + ")")
+  .attr("width", chartWidth )
+  .attr("height", chartHeight )
 
   var yScale = d3.scale.linear()
     .domain([0,
@@ -114,7 +122,8 @@ Template.workouts.rendered = function() {
   //console.log(dates);
 
   var xScale = d3.time.scale()
-    .domain([dates[0], d3.time.day.offset(dates[dates.length - 1], 1)])
+    .domain([ d3.time.day.offset(dates[0], - 2),
+             d3.time.day.offset(dates[dates.length - 1], 1)])
     .rangeRound([0, chartWidth]);
   
   //console.log('xscale ', xScale);
@@ -148,8 +157,7 @@ Template.workouts.rendered = function() {
     })
     .enter()
     .append("rect")
-    .attr("x", function(d, i) {
-      //console.log('d is ', d);
+    .attr("x", function(d, i) {      
       return xScale(d.createdAt);
     })
     .attr("y", function(d) {
@@ -162,41 +170,46 @@ Template.workouts.rendered = function() {
     })
     .attr("width", 30);
 
-//   svg
-//     .append("g")
-//     .attr("class", 'weight')
-//     .style("fill", "rgb(255,255,255)")
-//     .selectAll("text")
-//     .data(dataSet[0])
-//     .enter()
-//     .append("text")
-//     .text(function(d) {
-//       return 'Weight: ' + d.weight;
-//     })
-//     .attr("x", function(d, i) {
-//       return xScale(i);
-//     })
-//     .attr("y", function(d) {
-//       return chartHeight;
-//     });
 
-//   svg
-//     .append("g")
-//     .attr("class", 'reps')
-//     .style("fill", "rgb(255,255,255)")
-//     .selectAll("text")
-//     .data(dataSet[1])
-//     .enter()
-//     .append("text")
-//     .text(function(d) {
-//       return 'Reps: ' + d.reps;
-//     })
-//     .attr("x", function(d, i) {
-//       return xScale(i) + 75;
-//     })
-//     .attr("y", function(d) {
-//       return chartHeight;
-//     });
+
+ 
+
+  svg
+    .append("g")
+    .attr("class", 'weight')
+    .selectAll("text")    
+    .data(dataSet[0])
+    .enter()
+    .append("text")
+    .text(function(d) {
+      return 'Weight: ' + d.weight;
+    })
+    .attr("x", function(d, i) {
+      return xScale(d.createdAt);
+    })
+    .attr("y", function(d) {
+      return chartHeight;
+    })
+    .style("text-anchor", "end")
+       
+
+  // svg
+  //   .append("g")
+  //   .attr("class", 'reps')
+  //   .style("fill", "rgb(255,255,255)")
+  //   .selectAll("text")
+  //   .data(dataSet[1])
+  //   .enter()
+  //   .append("text")
+  //   .text(function(d) {
+  //     return 'Reps: ' + d.reps;
+  //   })
+  //   .attr("x", function(d, i) {
+  //     return xScale(d.createdAt);
+  //   })
+  //   .attr("y", function(d) {
+  //     return chartHeight;
+  //   });
 
 
 };
